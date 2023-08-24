c641573aa375b547bd1c29c11228b07345958675
indriynp <indriyanipertiwi2208@gmail.com>
2023-08-24 08:16:53 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,7 +45,8 @@
             </div>
         </div>
     </div>
-</body>     
+</body>
+
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
@@ -650,15 +651,14 @@
             </section>
             <!-- /.content -->
         </div>
-        
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-            All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+        <div id="content">
+                @include('includes.footer')
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
             </div>
-        </footer>
+        
+        
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
